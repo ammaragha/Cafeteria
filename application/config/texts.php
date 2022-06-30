1a43cf54fbf194cc0de41266659dfcc5a4b0b2aa
Ahmed Samir <a.samirsherif@gmail.com>
2022-06-30 23:53:11 +0200

    adding texts file for db model usage later

---
+++ application/config/texts.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Texts used in the application.
+ * These texts are used via Text::get('FEEDBACK_USERNAME_ALREADY_TAKEN').
+ * Could be extended to i18n etc.
+ */
+return array(
+    "FEEDBACK_TEST_TEXT" => "Text Test",
+);
