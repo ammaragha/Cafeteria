1614282df07cb32d64a4fc5aadd46abd236b25f4
Ahmed Samir <a.samirsherif@gmail.com>
2022-06-30 23:51:08 +0200

    adding page headers and footers

---
+++ application/view/app/header.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html>
+<head>
+    <title>Simple Cafe Ordering System</title>
+    <!-- META -->
+    <meta charset="utf-8">
+    <!-- send empty favicon fallback to prevent user's browser hitting the server for lots of favicon requests resulting in 404s -->
+    <link rel="icon" href="data:;base64,=">
+    <!-- CSS -->
+    <link rel="stylesheet" href="<?php echo Config::get('URL'); ?>css/style.css" />
+</head>
+<body>
+    <!-- wrapper, to center website -->
+    <div class="wrapper">
+
+        <!-- logo -->
+        <div class="logo"></div>
+
+        <!-- navigation -->
+        <ul class="navigation">
+            <!-- ADD LIST HERE -->
+        </ul>
+
+        <!-- my account -->
+        <ul class="navigation right">
+             <!-- ADD CURRENT USER HERE -->
+        </ul>
