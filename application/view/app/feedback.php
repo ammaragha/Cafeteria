a5debbc1c0c6b13874f699f10db343014df26a7f
Ahmed Samir <a.samirsherif@gmail.com>
2022-07-01 09:19:46 +0200

    toast notifications for success and errors

--- application/view/app/feedback.php
+++ application/view/app/feedback.php
@@ -7,13 +7,27 @@ $feedback_negative = Session::get('feedback_negative');
 // echo out positive messages
 if (isset($feedback_positive)) {
     foreach ($feedback_positive as $feedback) {
-        echo '<div class="feedback success">'.$feedback.'</div>';
+        echo '<div class="toast align-items-center text-white bg-primary border-0" role="alert" aria-live="assertive" aria-atomic="true">
+        <div class="d-flex">
+          <div class="toast-body">'
+          .$feedback.
+          '</div>
+          <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
+        </div>
+      </div>';
     }
 }
 
 // echo out negative messages
 if (isset($feedback_negative)) {
     foreach ($feedback_negative as $feedback) {
-        echo '<div class="feedback error">'.$feedback.'</div>';
+        echo '<div class="toast align-items-center text-white bg-danger border-0" role="alert" aria-live="assertive" aria-atomic="true">
+        <div class="d-flex">
+          <div class="toast-body">'
+          .$feedback.
+          '</div>
+          <button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
+        </div>
+      </div>';
     }
 }
