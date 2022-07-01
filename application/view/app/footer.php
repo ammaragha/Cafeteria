e8f1a528a37accc8491560ec7ba4f76eb7fbbf8c
Ahmed Samir <a.samirsherif@gmail.com>
2022-07-01 09:16:58 +0200

    adding js & css files to views

--- application/view/app/footer.php
+++ application/view/app/footer.php
@@ -1,4 +1,5 @@
 <div class="footer"></div>
     </div><!-- close class="wrapper" -->
+<script rel="stylesheet" href="<?php echo Config::get('URL'); ?>js/bootstrap.bundle.js"> </script>
 </body>
 </html>
