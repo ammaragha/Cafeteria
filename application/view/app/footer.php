4a06de528590d92de88a7c622bd1bbad4d2dc34a
Ahmed Samir <a.samirsherif@gmail.com>
2022-07-02 17:30:16 +0200

    editing layout

--- application/view/app/footer.php
+++ application/view/app/footer.php
@@ -1,5 +1,22 @@
 <div class="footer"></div>
     </div><!-- close class="wrapper" -->
-<script rel="stylesheet" href="<?php echo Config::get('URL'); ?>js/bootstrap.bundle.js"> </script>
+
+<!-- Bootstrap core JavaScript-->
+<script src="<?php echo Config::get('URL'); ?>vendor/jquery/jquery.min.js"></script>
+    <script src="<?php echo Config::get('URL'); ?>vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="<?php echo Config::get('URL'); ?>vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="<?php echo Config::get('URL'); ?>js/sb-admin-2.min.js"></script>
+
+    <!-- Page level plugins -->
+    <script src="<?php echo Config::get('URL'); ?>vendor/chart.js/Chart.min.js"></script>
+
+    <!-- Page level custom scripts -->
+    <script src="<?php echo Config::get('URL'); ?>js/demo/chart-area-demo.js"></script>
+    <script src="<?php echo Config::get('URL'); ?>js/demo/chart-pie-demo.js"></script>
+
 </body>
 </html>
--- application/view/app/header.php
+++ application/view/app/header.php
@@ -4,25 +4,23 @@
     <title>Simple Cafe Ordering System</title>
     <!-- META -->
     <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
     <!-- send empty favicon fallback to prevent user's browser hitting the server for lots of favicon requests resulting in 404s -->
     <link rel="icon" href="data:;base64,=">
     <!-- CSS -->
     <link rel="stylesheet" href="<?php echo Config::get('URL'); ?>css/bootstrap.css" />
+    <link href="<?php echo Config::get('URL'); ?>vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+    
+    <!-- Custom styling for SB 2 -->
+    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+
+    <!-- Custom styling login & register -->
     <link rel="stylesheet" href="<?php echo Config::get('URL'); ?>css/style.css" />
 </head>
 <body>
     <!-- wrapper, to center website -->
     <div class="wrapper">
-
-        <!-- logo -->
-        <div class="logo"></div>
-
-        <!-- navigation -->
-        <ul class="navigation">
-            <!-- ADD LIST HERE -->
-        </ul>
-
-        <!-- my account -->
-        <ul class="navigation right">
-             <!-- ADD CURRENT USER HERE -->
-        </ul>
