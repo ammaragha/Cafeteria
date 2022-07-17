231c2babb0cb3a2b20ffa43e447fc1fa4d943d92
Ahmed Samir <a.samirsherif@gmail.com>
2022-07-17 11:28:38 +0200

    tests-frontend

---
+++ test/ui/wp-admin/admin-ajax.php
@@ -0,0 +1,217 @@
+
+            <div class="vincent_prod_list_item">
+                <div class="vincent_prod_item_wrapper">
+                    <div class="vincent_prod_list_image_cont">
+                        <div class="vincent_prod_list_image_wrapper">
+                            
+                            
+                                        <a href="https://demo-storage.com/pm/wp/vincent/product/margherita/" class="vincent_prod_image_link ">
+                                            <span class="vincent_prod_list_overlay"></span>
+                                            <img src="https://demo-storage.com/pm/wp/vincent/wp-content/uploads/2017/03/11-600x600.gif" alt="" />
+                                        </a>
+                                    
+                            <p class="product woocommerce add_to_cart_inline " style="border: none;"><span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol">&#36;</span>2.60</span><a href="?add-to-cart=531" data-quantity="1" class="button product_type_simple add_to_cart_button ajax_add_to_cart" data-product_id="531" data-product_sku="A979-C" aria-label="Add &ldquo;Margherita&rdquo; to your cart" rel="nofollow">Add to cart</a></p>
+                        </div>
+                    </div>
+
+                    <h5><a href="https://demo-storage.com/pm/wp/vincent/product/margherita/">Margherita</a></h5>
+
+                    <p class="vincent_prod_list_excerpt">
+                        Classic marinara sauce, authentic old-world pepperoni, all-natural...
+                    </p>
+
+                    <div class="vincent_prod_list_price">
+                        <p class="product woocommerce add_to_cart_inline " style="border: none;"><span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol">&#36;</span>2.60</span><a href="?add-to-cart=531" data-quantity="1" class="button product_type_simple add_to_cart_button ajax_add_to_cart" data-product_id="531" data-product_sku="A979-C" aria-label="Add &ldquo;Margherita&rdquo; to your cart" rel="nofollow">Add to cart</a></p>
+                    </div>
+                </div>
+            </div>
+        
+            <div class="vincent_prod_list_item">
+                <div class="vincent_prod_item_wrapper">
+                    <div class="vincent_prod_list_image_cont">
+                        <div class="vincent_prod_list_image_wrapper">
+                            
+                            
+                                        <a href="https://demo-storage.com/pm/wp/vincent/product/vincent/" class="vincent_prod_image_link ">
+                                            <span class="vincent_prod_list_overlay"></span>
+                                            <img src="https://demo-storage.com/pm/wp/vincent/wp-content/uploads/2017/03/10-600x600.gif" alt="" />
+                                        </a>
+                                    
+                            <p class="product woocommerce add_to_cart_inline " style="border: none;"><span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol">&#36;</span>3.00</span> &ndash; <span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol">&#36;</span>16.00</span><a href="https://demo-storage.com/pm/wp/vincent/product/vincent/" data-quantity="1" class="button product_type_variable add_to_cart_button" data-product_id="544" data-product_sku="A935-C" aria-label="Select options for &ldquo;Vincent&rdquo;" rel="nofollow">Select options</a></p>
+                        </div>
+                    </div>
+
+                    <h5><a href="https://demo-storage.com/pm/wp/vincent/product/vincent/">Vincent</a></h5>
+
+                    <p class="vincent_prod_list_excerpt">
+                        Classic marinara sauce, authentic old-world pepperoni, all-natural...
+                    </p>
+
+                    <div class="vincent_prod_list_price">
+                        <p class="product woocommerce add_to_cart_inline " style="border: none;"><span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol">&#36;</span>3.00</span> &ndash; <span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol">&#36;</span>16.00</span><a href="https://demo-storage.com/pm/wp/vincent/product/vincent/" data-quantity="1" class="button product_type_variable add_to_cart_button" data-product_id="544" data-product_sku="A935-C" aria-label="Select options for &ldquo;Vincent&rdquo;" rel="nofollow">Select options</a></p>
+                    </div>
+                </div>
+            </div>
+        
+            <div class="vincent_prod_list_item">
+                <div class="vincent_prod_item_wrapper">
+                    <div class="vincent_prod_list_image_cont">
+                        <div class="vincent_prod_list_image_wrapper">
+                            
+                            
+                                        <a href="https://demo-storage.com/pm/wp/vincent/product/pepperoni/" class="vincent_prod_image_link ">
+                                            <span class="vincent_prod_list_overlay"></span>
+                                            <img src="https://demo-storage.com/pm/wp/vincent/wp-content/uploads/2017/03/2-600x600.gif" alt="" />
+                                        </a>
+                                    
+                            <p class="product woocommerce add_to_cart_inline " style="border: none;"><span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol">&#36;</span>2.90</span><a href="?add-to-cart=545" data-quantity="1" class="button product_type_simple add_to_cart_button ajax_add_to_cart" data-product_id="545" data-product_sku="A987-C" aria-label="Add &ldquo;Pepperoni&rdquo; to your cart" rel="nofollow">Add to cart</a></p>
+                        </div>
+                    </div>
+
+                    <h5><a href="https://demo-storage.com/pm/wp/vincent/product/pepperoni/">Pepperoni</a></h5>
+
+                    <p class="vincent_prod_list_excerpt">
+                        Classic marinara sauce, authentic old-world pepperoni, all-natural...
+                    </p>
+
+                    <div class="vincent_prod_list_price">
+                        <p class="product woocommerce add_to_cart_inline " style="border: none;"><span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol">&#36;</span>2.90</span><a href="?add-to-cart=545" data-quantity="1" class="button product_type_simple add_to_cart_button ajax_add_to_cart" data-product_id="545" data-product_sku="A987-C" aria-label="Add &ldquo;Pepperoni&rdquo; to your cart" rel="nofollow">Add to cart</a></p>
+                    </div>
+                </div>
+            </div>
+        
+            <div class="vincent_prod_list_item">
+                <div class="vincent_prod_item_wrapper">
+                    <div class="vincent_prod_list_image_cont">
+                        <div class="vincent_prod_list_image_wrapper">
+                            
+                            
+                                        <a href="https://demo-storage.com/pm/wp/vincent/product/diablo/" class="vincent_prod_image_link ">
+                                            <span class="vincent_prod_list_overlay"></span>
+                                            <img src="https://demo-storage.com/pm/wp/vincent/wp-content/uploads/2017/03/6-600x600.gif" alt="" />
+                                        </a>
+                                    
+                            <p class="product woocommerce add_to_cart_inline " style="border: none;"><span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol">&#36;</span>3.50</span><a href="?add-to-cart=546" data-quantity="1" class="button product_type_simple add_to_cart_button ajax_add_to_cart" data-product_id="546" data-product_sku="A666-C" aria-label="Add &ldquo;Diablo&rdquo; to your cart" rel="nofollow">Add to cart</a></p>
+                        </div>
+                    </div>
+
+                    <h5><a href="https://demo-storage.com/pm/wp/vincent/product/diablo/">Diablo</a></h5>
+
+                    <p class="vincent_prod_list_excerpt">
+                        Classic marinara sauce, authentic old-world pepperoni, all-natural...
+                    </p>
+
+                    <div class="vincent_prod_list_price">
+                        <p class="product woocommerce add_to_cart_inline " style="border: none;"><span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol">&#36;</span>3.50</span><a href="?add-to-cart=546" data-quantity="1" class="button product_type_simple add_to_cart_button ajax_add_to_cart" data-product_id="546" data-product_sku="A666-C" aria-label="Add &ldquo;Diablo&rdquo; to your cart" rel="nofollow">Add to cart</a></p>
+                    </div>
+                </div>
+            </div>
+        
+            <div class="vincent_prod_list_item">
+                <div class="vincent_prod_item_wrapper">
+                    <div class="vincent_prod_list_image_cont">
+                        <div class="vincent_prod_list_image_wrapper">
+                            
+                            
+                                        <a href="https://demo-storage.com/pm/wp/vincent/product/carbonara/" class="vincent_prod_image_link ">
+                                            <span class="vincent_prod_list_overlay"></span>
+                                            <img src="https://demo-storage.com/pm/wp/vincent/wp-content/uploads/2017/03/8-600x600.gif" alt="" />
+                                        </a>
+                                    
+                            <p class="product woocommerce add_to_cart_inline " style="border: none;"><span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol">&#36;</span>3.80</span><a href="?add-to-cart=583" data-quantity="1" class="button product_type_simple add_to_cart_button ajax_add_to_cart" data-product_id="583" data-product_sku="A599-C" aria-label="Add &ldquo;Carbonara&rdquo; to your cart" rel="nofollow">Add to cart</a></p>
+                        </div>
+                    </div>
+
+                    <h5><a href="https://demo-storage.com/pm/wp/vincent/product/carbonara/">Carbonara</a></h5>
+
+                    <p class="vincent_prod_list_excerpt">
+                        Classic marinara sauce, authentic old-world pepperoni, all-natural...
+                    </p>
+
+                    <div class="vincent_prod_list_price">
+                        <p class="product woocommerce add_to_cart_inline " style="border: none;"><span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol">&#36;</span>3.80</span><a href="?add-to-cart=583" data-quantity="1" class="button product_type_simple add_to_cart_button ajax_add_to_cart" data-product_id="583" data-product_sku="A599-C" aria-label="Add &ldquo;Carbonara&rdquo; to your cart" rel="nofollow">Add to cart</a></p>
+                    </div>
+                </div>
+            </div>
+        
+            <div class="vincent_prod_list_item">
+                <div class="vincent_prod_item_wrapper">
+                    <div class="vincent_prod_list_image_cont">
+                        <div class="vincent_prod_list_image_wrapper">
+                            
+                            
+                                        <a href="https://demo-storage.com/pm/wp/vincent/product/capricciosa/" class="vincent_prod_image_link ">
+                                            <span class="vincent_prod_list_overlay"></span>
+                                            <img src="https://demo-storage.com/pm/wp/vincent/wp-content/uploads/2017/03/4-600x600.gif" alt="" />
+                                        </a>
+                                    
+                            <p class="product woocommerce add_to_cart_inline " style="border: none;"><span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol">&#36;</span>2.90</span><a href="?add-to-cart=584" data-quantity="1" class="button product_type_simple add_to_cart_button ajax_add_to_cart" data-product_id="584" data-product_sku="A956-C" aria-label="Add &ldquo;Capricciosa&rdquo; to your cart" rel="nofollow">Add to cart</a></p>
+                        </div>
+                    </div>
+
+                    <h5><a href="https://demo-storage.com/pm/wp/vincent/product/capricciosa/">Capricciosa</a></h5>
+
+                    <p class="vincent_prod_list_excerpt">
+                        Classic marinara sauce, authentic old-world pepperoni, all-natural...
+                    </p>
+
+                    <div class="vincent_prod_list_price">
+                        <p class="product woocommerce add_to_cart_inline " style="border: none;"><span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol">&#36;</span>2.90</span><a href="?add-to-cart=584" data-quantity="1" class="button product_type_simple add_to_cart_button ajax_add_to_cart" data-product_id="584" data-product_sku="A956-C" aria-label="Add &ldquo;Capricciosa&rdquo; to your cart" rel="nofollow">Add to cart</a></p>
+                    </div>
+                </div>
+            </div>
+        
+            <div class="vincent_prod_list_item">
+                <div class="vincent_prod_item_wrapper">
+                    <div class="vincent_prod_list_image_cont">
+                        <div class="vincent_prod_list_image_wrapper">
+                            
+                            
+                                        <a href="https://demo-storage.com/pm/wp/vincent/product/prosciutto/" class="vincent_prod_image_link ">
+                                            <span class="vincent_prod_list_overlay"></span>
+                                            <img src="https://demo-storage.com/pm/wp/vincent/wp-content/uploads/2017/03/7-2-1-600x600.gif" alt="" />
+                                        </a>
+                                    
+                            <p class="product woocommerce add_to_cart_inline " style="border: none;"><span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol">&#36;</span>3.50</span><a href="?add-to-cart=586" data-quantity="1" class="button product_type_simple add_to_cart_button ajax_add_to_cart" data-product_id="586" data-product_sku="A783-C" aria-label="Add &ldquo;Prosciutto&rdquo; to your cart" rel="nofollow">Add to cart</a></p>
+                        </div>
+                    </div>
+
+                    <h5><a href="https://demo-storage.com/pm/wp/vincent/product/prosciutto/">Prosciutto</a></h5>
+
+                    <p class="vincent_prod_list_excerpt">
+                        Classic marinara sauce, authentic old-world pepperoni, all-natural...
+                    </p>
+
+                    <div class="vincent_prod_list_price">
+                        <p class="product woocommerce add_to_cart_inline " style="border: none;"><span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol">&#36;</span>3.50</span><a href="?add-to-cart=586" data-quantity="1" class="button product_type_simple add_to_cart_button ajax_add_to_cart" data-product_id="586" data-product_sku="A783-C" aria-label="Add &ldquo;Prosciutto&rdquo; to your cart" rel="nofollow">Add to cart</a></p>
+                    </div>
+                </div>
+            </div>
+        
+            <div class="vincent_prod_list_item">
+                <div class="vincent_prod_item_wrapper">
+                    <div class="vincent_prod_list_image_cont">
+                        <div class="vincent_prod_list_image_wrapper">
+                            
+                            
+                                        <a href="https://demo-storage.com/pm/wp/vincent/product/saporita/" class="vincent_prod_image_link ">
+                                            <span class="vincent_prod_list_overlay"></span>
+                                            <img src="https://demo-storage.com/pm/wp/vincent/wp-content/uploads/2017/03/9-600x600.gif" alt="" />
+                                        </a>
+                                    
+                            <p class="product woocommerce add_to_cart_inline " style="border: none;"><span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol">&#36;</span>2.60</span><a href="?add-to-cart=587" data-quantity="1" class="button product_type_simple add_to_cart_button ajax_add_to_cart" data-product_id="587" data-product_sku="A659-C" aria-label="Add &ldquo;Saporita&rdquo; to your cart" rel="nofollow">Add to cart</a></p>
+                        </div>
+                    </div>
+
+                    <h5><a href="https://demo-storage.com/pm/wp/vincent/product/saporita/">Saporita</a></h5>
+
+                    <p class="vincent_prod_list_excerpt">
+                        Classic marinara sauce, authentic old-world pepperoni, all-natural...
+                    </p>
+
+                    <div class="vincent_prod_list_price">
+                        <p class="product woocommerce add_to_cart_inline " style="border: none;"><span class="woocommerce-Price-amount amount"><span class="woocommerce-Price-currencySymbol">&#36;</span>2.60</span><a href="?add-to-cart=587" data-quantity="1" class="button product_type_simple add_to_cart_button ajax_add_to_cart" data-product_id="587" data-product_sku="A659-C" aria-label="Add &ldquo;Saporita&rdquo; to your cart" rel="nofollow">Add to cart</a></p>
+                    </div>
+                </div>
+            </div>
+        
